15f50e346febda752ddfa02e6df188f3c9b169be
Boarnasia <masato.uehara.1975@gmail.com>
2018-06-27 05:34:03 +0900

    add: newspics.com script

---
+++ newspics.com.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         NewsPics UI Enhancement
+// @namespace    http://yamashita109.pw/
+// @version      0.1
+// @description  Make NewsPics better.
+// @author       Boarnasia <masato.uehara.1975@gmail.com>
+// @match        https://newspicks.com/*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+
+
+(function() {
+    'use strict'
+
+    if (window.location.pathname.match(/^\/news\//)) return
+
+    const get_links = () => {
+        const links = $(`.menu .theme_menu a.menu-item`)
+        const ret = []
+        for(let i=0; i<links.length; i++) {
+            ret.push(links[i])
+        }
+        return ret
+    }
+    const links = get_links()
+
+    const elm_logo = $(`a.logo`)
+    const c = $(`<div></div>`)
+    c.attr({style: `display: flex;justify-content: center;align-items: center;height: 50px; width: 400px;`})
+    c.addClass("logo clear-index-cache")
+    elm_logo.before(c)
+    elm_logo.remove()
+
+    const a_style = `color: white;
+        width: 40px;
+        display: block;
+        text-align: center;
+        font-size: 200%;
+    `
+
+    const loc = window.location
+    let prev, next
+    prev = links[links.length - 1]
+    next = links[0]
+
+    const idx = links.findIndex(item => item.href === window.location.href)
+    if (idx == 0) {
+        next = links[idx + 1]
+    } else if (idx > 0) {
+        prev = links[idx - 1]
+        if (idx < (links.length-1)) {
+            next = links[idx + 1]
+        }
+    }
+
+    c.append($(`<a href="${prev.href}" title="${prev.text}" style="${a_style}">&laquo;</a>`))
+    c.append($(`<a href="/"><img style="margin-top: 0;" src="/images/logo_type.598a50a8.png"></a>`))
+    c.append($(`<a href="${next.href}" title="${next.text}" style="${a_style}">&raquo;</a>`))
+})();
